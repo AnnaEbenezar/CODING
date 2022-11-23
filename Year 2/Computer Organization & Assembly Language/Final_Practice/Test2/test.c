ba869a51f43606a02df379b6ed56ec735a2c87de
Hannah Ebenezar <91257701+AnnaEbenezar@users.noreply.github.com>
2022-11-23 15:59:08 +0700

    CO&AL Practice for Lab Final

---
+++ Year 2/Computer Organization & Assembly Language/Final_Practice/Test2/test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello World");
+    int c = 5 + 6;
+    printf("This is C: %d", c);
+
+    return 0;
+}
---
+++ Year 2/Computer Organization & Assembly Language/Final_Practice/Test2/test.s
@@ -0,0 +1,54 @@
+	.arch armv6
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"test.c"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Hello World\000"
+	.align	2
+.LC1:
+	.ascii	"This is C: %d\000"
+	.text
+	.align	2
+	.global	main
+	.arch armv6
+	.syntax unified
+	.arm
+	.fpu vfp
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #8
+	ldr	r0, .L3
+	bl	printf
+	mov	r3, #11
+	str	r3, [fp, #-8]
+	ldr	r1, [fp, #-8]
+	ldr	r0, .L3+4
+	bl	printf
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	pop	{fp, pc}
+.L4:
+	.align	2
+.L3:
+	.word	.LC0
+	.word	.LC1
+	.size	main, .-main
+	.ident	"GCC: (Raspbian 10.2.1-6+rpi1) 10.2.1 20210110"
+	.section	.note.GNU-stack,"",%progbits
