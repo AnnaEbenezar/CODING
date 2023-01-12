b3fc828ad315884aefa531b05f3881ec40fb049b
Hannah Ebenezar <91257701+AnnaEbenezar@users.noreply.github.com>
2023-01-12 13:56:31 +0700

    Y2; S1 - Final
    committing all the files from year 2; semester 1

---
+++ Year 2/AOOP/AOOP_Proj/src/main/java/com/mycompany/aoop_proj/ProfileDriver.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.aoop_proj;
+
+/**
+ *
+ * @author HP
+ */
+public class ProfileDriver {
+    
+    //@RestController
+    //@RequestMapping("/fileUpload")
+    private static class PicUpload {
+        
+    }
+}
+
+
